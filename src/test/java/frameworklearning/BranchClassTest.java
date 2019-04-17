c07ae504f37e06d54162191186bb61634789aaa2
chiragmangukia <chirag.mangukia@gmail.com>
2019-04-17 08:53:26 +0530

    Added a BranchClass classs

---
+++ src/test/java/frameworklearning/BranchClassTest.java
@@ -0,0 +1,12 @@
+package frameworklearning;
+
+import org.testng.annotations.Test;
+
+public class BranchClassTest {
+	
+	@Test
+	public void branchClass() {
+		System.out.println("Branch class method");
+	}
+
+}
